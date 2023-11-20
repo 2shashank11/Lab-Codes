0f1c1b74bcaf0e778358121d0b9be28606102db9
Shashank <76704277+2shashank11@users.noreply.github.com>
2023-11-20 06:24:19 +0530

    Add files via upload

---
+++ DS LAB/Lab 12/Q1.c
@@ -0,0 +1,126 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+typedef struct node{
+    int data;
+    struct node* left;
+    struct node* right;
+} node;
+
+node* createNode(int value){
+    node* temp=(node*)(malloc(sizeof(node)));
+    temp->data=value;
+    temp->left=temp->right=NULL;
+    return temp;
+}
+
+node* createTree(node* root, int value){
+    if(root==NULL){
+        return createNode(value);
+    }
+    node *p=root, *parent=NULL;
+    while(p!=NULL){
+        parent=p;
+        if(value < p->data){
+            p=p->left;
+        }
+        else if(value > p->data){
+            p=p->right;
+        }
+        else if(value==p->data){
+            return root;
+        }
+    }
+
+    node* temp = (node*)(malloc(sizeof(node)));
+    temp=createNode(value);
+    
+    if(value<parent->data){
+        parent->left=temp;
+    }
+    else{//if (value>parent->data)
+        parent->right=temp;
+    }
+
+    return root;
+}
+
+void inorder(node* root){
+    if(root==NULL) return;
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+node* inorderPredecessor(node* root){
+    root=root->left;
+    while(root->right!=NULL){
+        root=root->right;
+    }
+
+    return root;
+}
+
+node* deleteNode(node* root, int value){
+    if(root==NULL){
+        return NULL;
+    }
+
+    if(root->right==NULL && root->left==NULL){
+        free(root);
+        return NULL;
+    }
+
+    if(value < root->data){
+        root->left=deleteNode(root->left, value);
+    }
+
+    else if(value > root->data){
+        root->right=deleteNode(root->right, value);
+    }
+
+    else{
+        node* iPre = inorderPredecessor(root);
+        root->data=iPre->data;
+        root->left=deleteNode(root->left, iPre->data);
+    }
+
+    return root;
+}
+
+int main(){
+    int n, value, choice;
+    node* root=NULL;
+
+    printf("1. Insert \n2. Display \n3. Delete \n4. Exit \n");
+
+    while (true){
+        printf("\nChoice: ");
+        scanf("%d", &choice);
+
+        if(choice==1){
+            printf("Element: ");
+            scanf("%d", &value);
+            root=createTree(root, value);
+        }
+
+        else if(choice==2){
+            printf("\nIn-order: ");
+            inorder(root);
+            printf("\n");
+        }
+
+        else if(choice==3){
+            printf("Element to Delete: ");
+            scanf("%d", &value);
+            root=deleteNode(root, value);
+        }
+
+        else if(choice==4){
+            break;
+        }
+
+        else printf("\nSelect valid option!!\n");
+    }
+}
